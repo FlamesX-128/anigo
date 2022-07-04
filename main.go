d877d27f0342ac7513a17ee073322bed880490e4
FlamesX-128 <glowingorangeflames@gmail.com>
2022-07-04 17:45:53 -0500

    plugin loader, minimal changes.

--- main.go
+++ main.go
@@ -1,25 +1,33 @@
 package main
 
 import (
+	"os"
+	"path"
 	"sync"
 
+	"github.com/FlamesX-128/anigo/src/controllers/plugins"
 	"github.com/FlamesX-128/anigo/src/types"
+	"github.com/FlamesX-128/anigo/src/utils"
 )
 
 var wg sync.WaitGroup
 
 func main() {
-	aprops := types.Anigo_.Properties
+	plugins.Load(
+		path.Join(utils.Try(os.Getwd()), "plugins"),
+	)
 
-	processes := aprops["persistent-processes"].([]types.ProcessPlugin[int32])
-	fprocesses := aprops["floating-processes"].([]types.ProcessPlugin[float32])
+	var (
+		pprocesses = types.Anigo_.Properties["persistent-processes"]
+		fprocesses = types.Anigo_.Properties["floating-processes"]
+	)
 
-	for _, process := range fprocesses {
+	for _, process := range fprocesses.([]types.ProcessPlugin[float32]) {
 		go process.Handler(&types.Anigo_)
 
 	}
 
-	for _, process := range processes {
+	for _, process := range pprocesses.([]types.ProcessPlugin[int32]) {
 		go func() {
 			process.Handler(&types.Anigo_)
 			wg.Done()
